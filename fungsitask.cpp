c006bbe7269a677fe473834e85646850240d760a
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-02 14:49:43 +0700

    menambahkan semua isi prosedur input

--- fungsitask.cpp
+++ fungsitask.cpp
@@ -5,4 +5,10 @@ float Phi, X, r;
 // Implementasi fungsi dan prosedur
 void input()
 {
+    cout << "Masukan nilai Phi : ";
+    cin >> Phi;
+    cout << "Masukan nilai X : ";
+    cin >> X;
+    cout << "Masukan nilai r : ";
+    cin >> r;
 }
